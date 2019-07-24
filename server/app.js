1ff7f8e8f88d1edf31c2813a69a3656a12914b61
Lukas Babaliauskas <l.babaliauskas@gmail.com>
2019-07-23 22:33:41 -0400

    added mongoose, bookSchema, authorSchema

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express();
+
+mongoose.connect('mongodb://lukas:atsipalaidaves10@ds255107.mlab.com:55107/gql-ninja', { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+  console.log('Connected to DB')
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
